c6c3149a5d24b5a235681dbde7d3818fbb9ec326
BradlyLewis <bradlylewisdev@gmail.com>
2021-07-12 23:55:10 -0400

    update programmers.js

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -27,7 +27,7 @@ export const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
-  const [programmers, setProgrammers] = useState(listOfAwesome)
+  const [programmers] = useState(listOfAwesome)
   const [featured, setFeatured] = useState('')
 
   const getNameOfFeatured = () => {
@@ -36,7 +36,8 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    const person = programmers.find(({id}) => id === featured)
+
+    const person = programmers.find(({id}) => id === featured) // searches through the array to find the programmer with the same id of the clicked programmer and then returns the name key value
     return person.name
   };
 
